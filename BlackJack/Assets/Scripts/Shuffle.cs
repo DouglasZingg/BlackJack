ef87a979038300781cd72eb2e5be03e7177c457d
Douglas Zingg <douglaszingg@gmail.com>
2024-10-31 11:46:18 -0700

    Draw and Deal
    Implemented a draw and deal functions. It is simple but a good proof of concept I believe.
    Space to shuffle
    D to deal to dealer
    P to deal to Player 1

--- BlackJack/Assets/Scripts/Shuffle.cs
+++ BlackJack/Assets/Scripts/Shuffle.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 public class Shuffle : MonoBehaviour
 {
     public List<CardInfo> deck = new List<CardInfo>();
-
+    public GameObject newDeck;
+    public GameObject player;
+    public GameObject dealer;
+    private int index = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +24,32 @@ public class Shuffle : MonoBehaviour
         {
             ShuffleDeck(deck);
         }
+        if (Input.GetKeyDown("p"))
+        {
+            DrawCard(deck, player);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            DrawCard(deck, dealer);
+        }
     }
 
-    public void ShuffleDeck(List<CardInfo> temp)
+    public void ShuffleDeck(List<CardInfo> tempdeck)
     {
-        for (int i = temp.Count - 1; i >= 0; --i)
+        for (int count = tempdeck.Count - 1; count >= 0; --count)
         {
-            int j = UnityEngine.Random.Range(0, i + 1);
+            int tempIndex = UnityEngine.Random.Range(0, count + 1);
 
-            CardInfo tmp = temp[i];
-            temp[i] = temp[j];
-            temp[j] = tmp;
+            CardInfo tmp = tempdeck[count];
+            tempdeck[count] = tempdeck[tempIndex];
+            tempdeck[tempIndex] = tmp;
+            tempdeck[count].transform.parent = newDeck.transform;
         }
+    }
+
+    public void DrawCard(List<CardInfo> tempdeck, GameObject gameObject)
+    {
+        tempdeck[index].transform.parent = gameObject.transform;
+        ++index;
     }
 }
